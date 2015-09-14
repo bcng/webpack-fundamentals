90651604f64f05f2b75053525480b7ca32ae93c3
Ladrillo <Ladrillo@users.noreply.github.com>
2015-09-14 12:24:57 -0600

    Update development.js

--- core/server/config/env/development.js
+++ core/server/config/env/development.js
@@ -1,5 +1,5 @@
 // ENVIRONMENT VARIABLES
 module.exports = {
-    db: 'mongodb://user:******@ds043027.mongolab.com:43027/artsnstyle',
+    db: 'mongodb://****:******@ds043027.mongolab.com:43027/artsnstyle',
     sessionSecret: 'developmentSessionSecret'
 };
