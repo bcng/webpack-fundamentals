1fab0af67777003c643ae9541c6aa64e8c35e49e
Ladrillo <gabriel@cabrejas.net>
2015-09-13 20:27:15 -0600

    services work

--- core/client/app.js
+++ core/client/app.js
@@ -5,6 +5,7 @@ var uiRouter = require('angular-ui-router');
 
 var app = angular.module('app', [uiRouter]);
 
-require ('./home/home.controller')(app);
+require('./services/people.service.js')(app);
+require('./home/home.controller')(app);
 require('./routes')(app);
 
--- core/client/home/home.controller.js
+++ core/client/home/home.controller.js
@@ -1,5 +1,6 @@
 module.exports = function (app) {
-    app.controller('homeController', function ($scope) {
+    app.controller('homeController', function ($scope, peopleService) {
         $scope.test = "this is a motherfucking of the home view";
+        $scope.people = peopleService.people;
     });
 };
