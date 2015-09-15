21e071ec1173216bfb5e2f3c0212e6410596a2d2
Gabriel Cabrejas Poole <gabriel@cabrejas.net>
2015-09-14 18:53:42 -0600

    added comments

--- core/server/config/env/development.js
+++ core/server/config/env/development.js
@@ -1,5 +1,5 @@
 // ENVIRONMENT VARIABLES
 module.exports = {
-    db: 'mongodb://username:password@ds031903.mongolab.com:31903/example',
+    db: 'mongodb://username:password@ds031903.mongolab.com:31903/example', // use your own
     sessionSecret: 'developmentSessionSecret'
 };
--- core/server/config/env/production.js
+++ core/server/config/env/production.js
@@ -1,6 +1,6 @@
 // ENVIRONMENT VARIABLES
 // use your own mong
 module.exports = {
-    db: 'mongodb://username:password@ds031903.mongolab.com:31903/example',
+    db: 'mongodb://username:password@ds031903.mongolab.com:31903/example', // use your own
     sessionSecret: 'developmentSessionSecret'
 };
